eaf4ecfcaada758b10ca86efcd65b45b33281dfc
Wanambuko Ngoya Daniel <danielngoys@outlook.com>
2022-12-18 03:23:25 +0300

    users table

---
+++ db/migrate/20221218001722_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :email
+      t.string :username
+      t.string :pasword_digest
+      t.string :role, default: 'customer'
+
+      t.timestamps
+    end
+  end
+end

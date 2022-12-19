d5c1a235a3adf7f7cd5528dca1bdd32e97b9705f
Wanambuko Ngoya Daniel <danielngoys@outlook.com>
2022-12-19 16:07:29 +0300

    relationship between order hsitories and meals

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,4 +1,6 @@
 class Meal < ApplicationRecord
     has_many :category_meals
     has_many :categories, through: :category_meals
+    has_many :order_histories
+    has_many :orders, through: :order_histories
 end

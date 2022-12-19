9dcdb26cf782667d84a970ab402ef6729c5fcaa8
Wanambuko Ngoya Daniel <danielngoys@outlook.com>
2022-12-19 08:33:57 +0300

    categories table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_19_052327) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_19_053123) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,15 +21,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_19_052327) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "category_meals", force: :cascade do |t|
-    t.integer "category_id"
-    t.integer "meal_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "meals", force: :cascade do |t|
-    t.string "image_url"
+    t.string "name"
     t.text "description"
     t.integer "price"
     t.datetime "created_at", null: false

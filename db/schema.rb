2e964c7485713ec0fa4dffaace01908a1788721d
Wanambuko Ngoya Daniel <danielngoys@outlook.com>
2022-12-19 08:36:18 +0300

    meals and category join table

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_19_053123) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_19_053533) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.string "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "category_meals", force: :cascade do |t|
+    t.integer "category_id"
+    t.integer "meal_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

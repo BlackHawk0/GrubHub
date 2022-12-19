8e8dddb8b7a3eaa07e4d5ad20cb8b23d9b0949ed
Wanambuko Ngoya Daniel <danielngoys@outlook.com>
2022-12-19 15:53:06 +0300

    orders migration

--- db/migrate/20221219125108_create_orders.rb
+++ db/migrate/20221219125108_create_orders.rb
@@ -2,7 +2,7 @@ class CreateOrders < ActiveRecord::Migration[7.0]
   def change
     create_table :orders do |t|
       t.integer :total_prize
-      t.string :status
+      t.string :status, default: 'pending'
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_19_124840) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_19_125108) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -38,7 +38,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_19_124840) do
 
   create_table "orders", force: :cascade do |t|
     t.integer "total_prize"
-    t.string "status"
+    t.string "status", default: "pending"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

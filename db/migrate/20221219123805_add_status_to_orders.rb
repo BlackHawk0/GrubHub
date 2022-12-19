65dd3adea833c859a959dc0704ddaea3b7a366a3
Wanambuko Ngoya Daniel <danielngoys@outlook.com>
2022-12-19 15:49:23 +0300

    orders migration

--- db/migrate/20221219123805_add_status_to_orders.rb
+++
@@ -1,5 +0,0 @@
-class AddStatusToOrders < ActiveRecord::Migration[7.0]
-  def change
-    add_column :orders, :status, :string, default: 'pending'
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_19_123805) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_19_124840) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,15 +37,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_19_123805) do
   end
 
   create_table "orders", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "meal_id", null: false
-    t.integer "quantity"
-    t.integer "total_price"
+    t.integer "total_prize"
+    t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "status", default: "pending"
-    t.index ["meal_id"], name: "index_orders_on_meal_id"
-    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -57,6 +52,4 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_19_123805) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "orders", "meals"
-  add_foreign_key "orders", "users"
 end

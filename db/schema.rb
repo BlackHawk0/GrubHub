24e4aa29e08e8c0abfd28c50fca9ecb895484684
Wanambuko Ngoya Daniel <danielngoys@outlook.com>
2022-12-28 23:00:49 +0300

    added user id for association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_19_125948) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_28_183701) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -50,6 +50,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_19_125948) do
     t.string "status", default: "pending"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "order_id"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|

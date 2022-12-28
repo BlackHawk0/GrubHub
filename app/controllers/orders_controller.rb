d5a471764dba956bf35a631b3e862099900e1d73
Wanambuko Ngoya Daniel <danielngoys@outlook.com>
2022-12-28 22:58:48 +0300

    admin order creation association

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,16 +8,23 @@ class OrdersController < ApplicationController
 
     # POST /orders
     def create 
-        order = Order.create!(order_params)
+        # byebug
+        user = User.find(params[:user_id])
+        # order = user.orders.create!(order_params)
+        order = Order.create!(order_params.merge(user: user))
+        order.order_id = order.generate_order_id
         render json: order, status: :created
     end
 
     private
     def authorize_customer
-        render json: { error: 'Not Authorized' }, status: :unprocessable_entity unless current_user.user_type == 'customer'
+        render json: { error: 'Not Authorized' }, status: :unprocessable_entity unless current_user.role == 'customer'
     end
 
     def order_params
-        params.require(:order).permit(:total_price, :status)
+        params.require(:order).permit(:total_prize, :status)
     end
+
+   
+
 end

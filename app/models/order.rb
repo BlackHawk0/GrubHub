331c11a00bce7e48d8c7fc35f0d12693baf0ac2b
Wanambuko Ngoya Daniel <danielngoys@outlook.com>
2022-12-28 22:59:23 +0300

    order id generation (hex)

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,11 @@ class Order < ApplicationRecord
   belongs_to :user
   has_many :order_histories
   has_many :meals, through: :order_histories
+
+  def generate_order_id
+      loop do
+          order_id = SecureRandom.hex(3) # generates a random string of hexadecimal characters
+          break order_id unless Order.exists?(order_id: order_id) # check if the generated order ID is already in use
+      end
+  end
 end

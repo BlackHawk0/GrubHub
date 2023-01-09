034d618e2837008fa239832324759e7d2259accb
Daniel <danielngoys@outlook.com>
2023-01-09 11:20:15 +0300

    get user order history for all delivered and completed orders

--- app/controllers/order_histories_controller.rb
+++ app/controllers/order_histories_controller.rb
@@ -1,10 +1,14 @@
 class OrderHistoriesController < ApplicationController
-    
+
+    # Get all orders that have been placed by the current user
     def index
         orders = current_user.orders.where.not(status: 'pending')
-        render json: orders, status: 200
+        render json: orders, status: :ok
     end
 
-    def sales
-    end
+    # def history
+    #     orders = OrderHistory.all
+    #     render json: orders, status: :ok
+    # end
+
 end

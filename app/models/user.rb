b89730cec8f81bdd6157540e402cb044b7b9267b
Daniel <danielngoys@outlook.com>
2023-01-12 19:39:20 +0300

    instance method to get user role

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,8 @@ class User < ApplicationRecord
     validates :username, presence: true, uniqueness:true
     validates :password, presence: true, format: { with: /\A(?=.*?[A-Z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{6,}\z/, 
     message: "must contain at least 1 uppercase letter, 1 numeric character, 1 special character, and must be 6 characters in length" }
+
+    def role?(role)
+        self.role == role.to_s
+    end
 end

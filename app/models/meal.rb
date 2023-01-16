b40703bbab1e21c8a9b5a99285eb0e125478f528
Daniel <danielngoys@outlook.com>
2023-01-16 12:42:13 +0300

    updated meals associations

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,6 +1,7 @@
 class Meal < ApplicationRecord
-    has_many :category_meals
-    has_many :categories, through: :category_meals
+    # has_many :category_meals
+    belongs_to :category
+    # has_many :categories, through: :category_meals
     has_many :order_histories
     has_many :orders, through: :order_histories
 end

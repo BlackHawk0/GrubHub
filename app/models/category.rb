d388b250ed9e4e53aa497c8ea894496c307c57b3
Daniel <danielngoys@outlook.com>
2023-01-16 12:42:01 +0300

    updated category associations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,5 @@
 class Category < ApplicationRecord
     validates :name, presence: true
     validates :image_url, presence: true
-    has_many :category_meals
-    has_many :meals, through: :category_meals
+    has_many :meals
 end

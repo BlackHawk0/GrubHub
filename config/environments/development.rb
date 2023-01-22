0d14d5fd597dbdff6f1554ed3811f5f76a3b4682
Daniel <danielngoys@outlook.com>
2023-01-22 13:58:38 +0300

    mpesa daraja api configuration setup

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,4 +54,9 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  #daraja api configurations
+  # config.hosts << /[a-z0-9]+\.ngrok\.io/
+  config.hosts.clear
+  # config.hosts << /[a-z0-9-.]+\.ngrok\.io/
 end
